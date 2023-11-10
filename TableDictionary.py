99d6411c43b66c5752aee82743f5ad15e32b896e
GenarAltinisik <79258317+GenarAltinisik@users.noreply.github.com>
2023-11-10 02:52:46 +0100

    Update TableDictionary.py

--- TableDictionary.py
+++ TableDictionary.py
@@ -1,6 +1,6 @@
 table_dict = {
-  "table 1 ": "test",
-  "table 2 ": "test",
-  "table 3 ": "test",
-  "table 4 ": "test"
+  "table 1": "test",
+  "table 2": "test",
+  "table 3": "test",
+  "table 4": "test"
 }
